cb734723b8cdb63d0a39466755d52e89742843ca
Eli Pandolfo <elipandolfo@gmail.com>
2017-03-22 12:03:57 -0700

    Add files via upload
    tablesort is an incomplete prototype that is highly efficient in scenarios where no int to be sorted is very large, and horribly inefficient when an int is large.

---
+++ InsertionSort.java
@@ -0,0 +1,31 @@
+
+
+public class InsertionSort
+{
+  static int[] A = {};
+
+  public static void main(String[] args)
+  {
+    sort(A);
+    // prints array
+    for(int e: A)
+      System.out.print(e + " ");
+    System.out.println();
+  }
+
+  public static void sort(int[] A)
+  {
+    for(int i=1; i<A.length; i++)
+    {
+      int j=i;
+      while(j >= 1 && A[j-1] > A[j])
+      {
+        int temp = A[j];
+        A[j] = A[j-1];
+        A[j-1] = temp;
+
+        j--;
+      }
+    }
+  }
+}
